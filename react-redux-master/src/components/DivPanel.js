13bb37e6b7a23c27a05ad0791e7c61eb1afa9fec
Jay Kalbi <jdkalbi18@gmail.com>
2024-02-25 15:02:09 +0530

    Update DivPanel.js

--- react-redux-master/src/components/DivPanel.js
+++ react-redux-master/src/components/DivPanel.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { useSelector } from "react-redux";
 
+const DivPanel = () => {
+    let counterval = useSelector(state => state.counter)
+    return (
+        <div>
+            The present value of counter is {counterval}
+        </div>
+    );
+}
+
+export default DivPanel;
